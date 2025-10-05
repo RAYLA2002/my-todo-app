2843e75243a1868ac31ef842ad3beb273a4f60bc
RAYLA2002 <amirhoseinkhanjany@gmail.com>
2025-10-06 03:29:13 +0330

    Update web.py

--- web.py
+++ web.py
@@ -44,14 +44,14 @@ st.markdown("""
     </style>
     """, unsafe_allow_html=True)
 
-st.title("🎯 اپلیکیشن مدیریت وظایف من")
+st.title("🎯اپلیکیشن مدیریت وظایف ")
 st.markdown("### **:blue[بهره‌وری خود را افزایش دهید!]**")
 st.write("وظایف خود را با نظم و ترتیب مدیریت کنید. برای تکمیل شدن یک وظیفه، تیک آن را بزنید.")
 
 st.divider()
 
 if not todos:
-    st.info("🎉 لیست وظایف شما خالی است! یک کار جدید اضافه کنید.")
+    st.info("لیست وظایف شما خالی است.")
 
 for index, todo in enumerate(todos):
     with st.container(border=True): 
@@ -74,7 +74,7 @@ with col1:
     # این ورودی حالا فقط مقدار را در session_state ذخیره می‌کند.
     st.text_input(
         label='افزودن وظیفه جدید:', 
-        placeholder="مثلاً: مستندسازی پروژه را تمام کنم...",
+        placeholder="وظایف خود را وارد کنید: ...",
         key="new_input", # کلید تغییر کرد
         label_visibility="collapsed"
     )
